cbb158d9bac60bd97246d9fb70bba5f2903538d4
Ace <hatkun.2005@gmail.com>
2025-12-24 23:58:07 +0900

    first commit

---
+++ 2-12/src/Main4.java
@@ -0,0 +1,9 @@
+public class Main4 {
+    public static void main(String[] args) {
+        int num = 10;
+        while (num >= 0) {
+            System.out.println("Countdown: " + num);
+            num--;
+        }
+    }
+}
---
+++ 2-12/src/Main5.java
@@ -0,0 +1,8 @@
+public class Main5 {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("現在の在庫数: " + i);
+        }
+        System.out.println("在庫がなくなりました。");
+    }
+}
