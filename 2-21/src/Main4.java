427c261cd296b821cbc546423843f6d9a94680d4
Ace <hatkun.2005@gmail.com>
2026-01-29 13:20:13 +0900

    修正

--- 2-21/src/Main4.java
+++ 2-21/src/Main4.java
@@ -3,15 +3,18 @@ import java.util.Collections;
 
 public class Main4 {
     public static void main(String[] args) {
-        var list = new ArrayList<Integer>();
-        Collections.addAll(list,5,2,8,1,9);
-        System.out.printf("元のリスト：%s%n",list);
-        processNumbers(list);
+        int[] arr = {5,2,8,1,9};
+        processNumbers(arr);
     }
-    static void processNumbers(ArrayList<Integer> numbers){
-        Collections.sort(numbers);
-        System.out.printf("ソート後：%s%n",numbers);
-        System.out.printf("最大値：%d%n",Collections.max(numbers));
-        System.out.printf("最小値：%d%n",Collections.min(numbers));
+    static void processNumbers(int[] numbers){
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int n : numbers){
+            list.add(n);
+        }
+        System.out.printf("元のリスト：%s%n",list);
+        Collections.sort(list);
+        System.out.printf("ソート後：%s%n",list);
+        System.out.printf("最大値：%d%n",Collections.max(list));
+        System.out.printf("最小値：%d%n",Collections.min(list));
     }
 }
